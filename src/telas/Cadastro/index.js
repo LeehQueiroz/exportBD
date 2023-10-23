6c0bf4a0beb45df23fc8b9f4ac3b0ee705d3d7be
Aula AndroidStudio <aula.android@fatectq.local>
2023-10-23 11:15:18 -0300

    atualizado 23/10

---
+++ src/telas/Cadastro/index.js
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from 'react';
+import { View } from 'react-native';
+import Botao from '../../componentes/Botao';
+import { EntradaTexto } from '../../componentes/EntradaTexto';
+import estilos from './estilos';
+import { cadastrar } from '../../servicos/requisicoesFirebase';
+import { auth } from '../../config/firebase';
+import { createUserWithEmailAndPassword } from "firebase/auth";
+
+export default function Cadastro({ navigation }) {  
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+  const [confirmaSenha, setConfirmaSenha] = useState('');
+
+  useEffect(()=>{
+    createUserWithEmailAndPassword(auth, "teste2@gmail.com", "12345678", null)
+    .then((dadosUsuario)=>{
+      console.log(dadosUsuario);
+    })
+    .catch((error)=>{
+      const errorCode = error.code;
+      const erroMessage = error.message;
+      console.log(error);
+      // ...
+    })
+  })
+
+  async function realizarCadastro(){
+    await cadastrar(email, senha, confirmaSenha);
+    setEmail('')
+    setSenha('')
+    setConfirmaSenha('')
+  }
+
+
+
+  return (
+    <View style={estilos.container}>
+      <EntradaTexto 
+        label="E-mail"
+        value={email}
+        onChangeText={texto => setEmail(texto)}
+      />
+      <EntradaTexto
+        label="Senha"
+        value={senha}
+        onChangeText={texto => setSenha(texto)}
+        secureTextEntry
+      />
+
+      <EntradaTexto
+        label="Confirmar Senha"
+        value={confirmaSenha}
+        onChangeText={texto => setConfirmaSenha(texto)}
+        secureTextEntry
+      />
+      
+      <Botao onPress={() => realizarCadastro()}>CADASTRAR</Botao>
+    </View>
+  );
+}
